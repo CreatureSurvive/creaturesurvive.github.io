caabd691ed8fe402ecbba8949ef171b5f0383705
CreatureSurvive <cs@creaturecoding.com>
2017-10-02 01:18:50 -0500

    Added libCSWeather

---
+++ scripts/update-local.sh
@@ -0,0 +1,29 @@
+# @Author: Dana Buehre <creaturesurvive>
+# @Date:   20-03-2017 2:10:27
+# @Email:  dbuehre@me.com
+# @Filename: update-local.sh
+# @Last modified by:   creaturesurvive
+# @Last modified time: 02-10-2017 1:13:29
+# @Copyright: Copyright © 2014-2017 CreatureSurvive
+
+
+#!/bin/bash
+
+#clean up
+#find ./debs -type f -name '*.deb' -delete
+#rm -r Packages.bz2
+
+#debs
+# dpkg-deb --bZlzma Projects/<projectname> <output folder>
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/motuumls/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/fastdel/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/flatfields/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/intelligentpower/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/nodropsheet/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/predictionstickaround/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/sera/.theos/_ debs
+dpkg-deb -bZlzma ~/Projects/Theos/tweaks/libCSWeather/.theos/_ debs
+
+#packages
+dpkg-scanpackages -m ./debs > Packages
+bzip2 -fks Packages
