ff1f19887bbbb9595f97ba863fa022d6c7b29d4e
CreatureSurvive <cs@creaturecoding.com>
2018-05-06 11:32:14 -0500

    rebuilding Packages 'date'

---
+++ scripts/update-ios.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#checks for root
+if [ `id -u` != 0 ]; then echo ""Run this as root""; exit 0; fi
+
+#build debs
+echo building debs
+dpkg-deb -b /var/mobile/dev/Repositories/ReachCC/.theos/_ debs
+
+#packages
+echo building Packages
+dpkg-scanpackages -m . /dev/null >Packages
+
+#compress
+echo compressing Packages
+bzip2 -fks Packages
+
+#git
+git add .
+
+msg="rebuilding Packages 'date'"
+if [$# -eq 1]
+	then msg="$1"
+fi
+git commit -m "$msg"
+
+echo pushing to master
+git push origin master
