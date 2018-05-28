c5191b4647b84875ea09a2fbb344e70329f7d4a8
Dana Buehre <cs@creaturecoding.com>
2018-05-28 13:23:30 -0500

    rebuilding Packages

--- scripts/update-ios.sh
+++ scripts/update-ios.sh
@@ -11,7 +11,7 @@ dpkg-deb -b /var/mobile/dev/Repositories/simmerdown/.theos/_ debs
 
 #packages
 echo building Packages
-scanpackages -m . /dev/null >Packages
+$THEOS/bin/scanpackages -m . /dev/null >Packages
 
 #compress
 echo compressing Packages
