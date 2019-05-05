ffb2aca1af15e4ba394501c3c8e36ec2ae1c04b4
Dana Buehre <cs@creaturecoding.com>
2019-05-05 16:06:58 -0500

    added AddRepo pacakge

--- scripts/update-desktop.sh
+++ scripts/update-desktop.sh
@@ -11,35 +11,36 @@
 
 #build debs
 echo building debs
-# dpkg-deb -b Projects/<projectname> <output folder>
-# dpkg-deb -b /Volumes/data/Projects/Git/motuumls/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/fastdel/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/flatfields/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/intelligentpower/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/nodropsheet/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/predictionstickaround/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/sera/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/libCSWeather/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/foresight/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/switcherRadii/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/alwaysPlaying/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/selectivecontrols/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/libCSColorPicker/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/libCSPreferences/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/pulltoclear/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/Consolidation/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/reachalert/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/copyvideourl/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/attibutionless/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/ToothModule/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/switcherRadiiXI/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/stretcher/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/shuffle/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/ebullience/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/simmerdown/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/crux/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/covert/.theos/_ debs
-# dpkg-deb -b /Volumes/data/Projects/Git/bubbles/.theos/_ debs
+# dpkg-deb -bZlzma Projects/<projectname> <output folder>
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/motuumls/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/fastdel/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/flatfields/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/intelligentpower/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/nodropsheet/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/predictionstickaround/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/sera/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/libCSWeather/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/foresight/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/switcherRadii/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/alwaysPlaying/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/selectivecontrols/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/libCSColorPicker/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/libCSPreferences/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/pulltoclear/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/Consolidation/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/reachalert/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/copyvideourl/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/attibutionless/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/ToothModule/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/switcherRadiiXI/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/stretcher/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/shuffle/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/ebullience/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/simmerdown/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/crux/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/covert/.theos/_ debs
+# dpkg-deb -bZlzma /Volumes/data/Projects/Git/bubbles/.theos/_ debs
+# dpkg-deb -b /Volumes/data/Projects/Git/addrepo/.theos/_ debs
 
 #packages
 echo building Packages
